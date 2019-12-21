0777234991f98d7de5467a4dc03f1cee631c93e3
429272-16 <429272-16>
2019-12-21 11:58:06 +0300

    Zad2

--- index.php
+++ index.php
@@ -1,20 +1,37 @@
 <?php
-class B {
 
-}
-Class ClassA extends ClassB {
-	public function _construcct ($obj1, $obj2,$obj3) {
-		$this->$obj1 = $obj1;
-		$this->$obj2 = $obj2;
-		$this->$obj3 = $obj3;
+Class linear1 {			//aX+b = 0
+	protected function linear($a, $b){
+		if ($a==0) {
+			return false;
+		}
+		return 	$this->x = (-1*$b)/$c;
 	}
-	public $obj1;
-	public $obj2;
-	public $obj3;
+	protected $x ;
+}			
+
+Class square1 extends linear1 {
+	protected function Disc($a,$b,$c){
+		return pow($b,2) - 4*$a*$c;
+	}
+	protected function square($a, $b, $c) {
+		if ($a==0) {
+			return $x = (-1*$c)/$b;
+		}
+		 else {
+			if ($disc>0) {
+				$x1 = (-1*$b+sqrt($disc))/(2*$a);
+				$x2 = (-1*$b-sqrt($disc))/(2*$a);
+			} elseif ($d==0) {
+				$x1 = $x2 = (-$b)/(2*$a);
+			} else {
+				$x1 = -1*$b + sqrt(abs($disc));
+				$x2 = -1*$b - sqrt(abs($disc));
+			}
+		}
+		return array($x1, $x2);
+}	
+protected $x1;
+protected $x2;
 }
-$obj1 = new ClassB();
-$obj2 = new ClassB();
-$obj3 = new ClassB();
-$obj4 = new ClassA($obj1);
-$obj5 = new ClassA($obj2, $obj3, $obj4);
 ?>
